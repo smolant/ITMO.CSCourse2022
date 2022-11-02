dfa1b4406af61dcacafaf093f455e221a2816dd7
smola <smola@MYNOTE>
2022-11-02 19:04:02 +0100

    Add project files.

---
+++ ITMO.CSCourse2022.FirstConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            int c = a + b;
+
+            //Console.WriteLine("Hello, World");
+            Console.WriteLine("Result = {0}", c);
+
+            Console.ReadKey();
+        }
+    }
+}
